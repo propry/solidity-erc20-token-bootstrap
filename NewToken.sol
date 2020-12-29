53439d3d53f261893d613efe9fd713898617be81
propry <76742615+propry@users.noreply.github.com>
2020-12-29 19:35:16 +0300

    Prophet.sol

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
+		totalSupply = 100000000;
+		name = "Prophet";
 		decimals = 4;
-		symbol = "BEC";
+		symbol = "PRY";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
